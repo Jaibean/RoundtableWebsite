5154a7db004ee575ca1be39e57d1d11b0fa747f1
Jaibean <jaimie.bertoli@gmail.com>
2021-05-19 21:20:44 -0400

    updates to coding challenge
    roundtable weekly meeting

--- scripts/May12thChallenge.js
+++ scripts/May12thChallenge.js
@@ -3,8 +3,13 @@
 // The binary number returned should be a string.
 
 function addBinary(num1,num2) {
-    //write code here
+    return (num1 + num2).toString(2);
+
 }
 
+// console.log(addBinary(500,3200000))
+
 //Output to window here:
-document.getElementById("result-box").innerHTML = ''
+document.getElementById("result-box").innerHTML = `512 + 200 in binary = ${addBinary(512,200)}<br>
+17 + 19 in binary = ${addBinary(17,19)}<br>
+3000 + 487 in binary = ${addBinary(3000,487)}`
--- scripts/main.js
+++ scripts/main.js
@@ -6,18 +6,18 @@
 //dynamically load script tag into head tag based on option selected from dropdown
 function selectChallenge() {
     var file = document.getElementById('options').value;
+    if (document.head.lastChild.id == "challenge-script"){
+        document.head.lastChild.remove();
+    }
     var newScriptTag = document.createElement('script');
-
-    for (i=0; i < file.length; i++) {
-        if (file[i] > 1) {
+    newScriptTag.id = 'challenge-script';
     newScriptTag.src = `scripts/${file}.js`;
     document.head.appendChild(newScriptTag);
-        }
-    }
 }
 
 function clearResult() {
     document.getElementById("result-box").innerHTML = '';
     document.getElementById('options').value = "-- Select Date --";
+    var newScriptTag = document.getElementById('challenge-script').remove();
 }
 
