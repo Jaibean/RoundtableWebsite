6e401c281d7632a94bfa9f8bd1edeaf50cdd55ac
Jaibean <jaimie.bertoli@gmail.com>
2021-06-30 09:04:29 -0400

    challenge updates

--- scripts/May26thChallenge.js
+++ scripts/May26thChallenge.js
@@ -6,9 +6,15 @@
 
 // If 27 is our input, we should return false because swapping the digits gives us 72, and 72 > 27. On the other hand, swapping 43 gives us 34, and 43 > 34.
 
-largestSwap(num){
-//your code goes here
+function largestSwap(num) {
+    var swapped = num.toString().split('').reverse().join('');
+    return [swapped, swapped <= num];
 }
 
-Output to window here:
-document.getElementById("result-box").innerHTML =
+//Output to window here:
+
+var num = 98
+
+document.getElementById("instructions-text").innerHTML = instructions;
+document.getElementById("example-text").innerHTML = example;
+document.getElementById("result-box").innerHTML = `${num} is larger than ${largestSwap(num)[0]}?: ${largestSwap(num)[1]}`; 
